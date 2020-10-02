bb2e31b469cb9bfd1a9d36b4c97485247bdee6c6
Joel Oliveira <joeldorosarioo@gmail.com>
2020-10-02 16:08:44 -0300

    Creates the ChannelList component

---
+++ src/components/ChannelList/ChannelButton.vue
@@ -0,0 +1,95 @@
+<template>
+	<div class="channel-button" :class="{ active: selected }">
+		<div class="channel-info">
+			<HashTagIcon class="hash-icon" :size="20" />
+
+			<div>{{ channelName }}</div>
+		</div>
+
+		<div class="channel-actions">
+			<InviteIcon class="icon-actions" :size="16" />
+			<SettingsIcon class="icon-actions" size="16" />
+		</div>
+	</div>
+</template>
+
+<script>
+	import HashTagIcon from 'vue-material-design-icons/Pound';
+	import InviteIcon from 'vue-material-design-icons/AccountPlus';
+	import SettingsIcon from 'vue-material-design-icons/CogOutline';
+
+	export default {
+		components: {
+			HashTagIcon,
+			InviteIcon,
+			SettingsIcon,
+		},
+		props: {
+			channelName: String,
+			selected: Boolean,
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.channel-button {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		cursor: pointer;
+		padding: 5px 8px;
+		background-color: transparent;
+
+		height: 32px;
+		border-radius: 4px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		transition: ease .2s;
+
+		> div {
+			display: flex;
+		}
+
+		.channel-info {
+			div {
+				color: var(--senary);
+				margin-left: 5px;
+			}
+
+			.hash-icon {
+				color: var(--symbol);
+			}
+		}
+
+		.channel-actions {
+			display: none;
+
+			.icon-actions {
+				color: var(--symbol);
+				margin-left: 4px;
+				margin-top: 2px;
+				transition: ease .2s;
+
+				&:hover {
+					color: var(--white);
+				}
+			}
+		}
+
+		&:hover, &.active {
+			background-color: var(--quinary);
+
+			.channel-info {
+				div {
+					color: var(--white);
+				}
+			}
+
+			.channel-actions {
+				display: flex;
+			}
+		}
+	}
+</style>
---
+++ src/components/ChannelList/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="channel-list">
+		<div class="category">
+			<div>Canais de Texto</div>
+
+			<div class="category-icon">
+				<AddCategoryIcon :size="21" />
+			</div>
+		</div>
+
+		<ChannelButton channelName="javascript-vanilla" />
+		<ChannelButton selected channelName="vue-js" />
+		<ChannelButton channelName="react-js" />
+		<ChannelButton channelName="typescript" />
+		<ChannelButton channelName="angular" />
+	</div>
+</template>
+
+<script>
+	import AddCategoryIcon from 'vue-material-design-icons/Plus';
+	import ChannelButton from './ChannelButton';
+
+	export default {
+		components: {
+			AddCategoryIcon,
+			ChannelButton,
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.channel-list {
+		grid-area: channel-list;
+		display: flex;
+		flex-direction: column;
+		padding: 24px 10px 0 16px;
+		background-color: var(--secondary);
+	}
+
+	.category {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 6px;
+		color: var(--grey);
+		font-size: 12px;
+		font-weight: bold;
+		text-transform: uppercase;
+
+		.category-icon {
+			color: var(--symbol);
+			cursor: pointer;
+			margin-right: 4px;
+
+			&:hover {
+				color: var(--white);
+			}
+		}
+	}
+</style>
